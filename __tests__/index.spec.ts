c31a6d9b6d239fab610ec1507e89bf463e0709f7
Diamondyuan <admin@diamondyuan.com>
2018-11-12 13:07:59 +0800

    🎉 Initialize Repo

---
+++ __tests__/index.spec.ts
@@ -0,0 +1,19 @@
+import { parser } from '../src/index';
+
+//命令行输入参数转 args 数组
+function argsCreater(input: string): string[] {
+  return ['', ''].concat(
+    input.split(' ').filter((o: string) => {
+      return o;
+    }),
+  );
+}
+
+describe('测试参数解析', () => {
+  it('测试参数解析的功能', () => {
+    expect(parser.parse(argsCreater('-T')).typescript).toBe(true);
+    expect(parser.parse(argsCreater('--host https://www.qq.com')).host).toBe(
+      'https://www.qq.com',
+    );
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import * as commander from 'commander';
+
+const parser = commander
+  .version('0.0.1')
+  .option('-T, --typescript', '输出的文件为 Typescript')
+  .option('--host  [value]', '静态文件所在的域名')
+  .option('--limit [value]', '低于指定的限制时，可以返回 Base64。默认为 10k')
+  .option('--test [value]', '需要的文件格式')
+  .option('--ignore [value]', '忽略的文件格式')
+  .option('--target [value]', '目标文件')
+  .option('--dist [value]', '输出文件夹');
+
+export interface IOption {
+  typescript: boolean;
+}
+
+const defaultOption: IOption = {
+  typescript: false,
+};
+
+/**
+ * 读取静态文件
+ */
+class WxappImageLoader {
+  /**
+   * 用户的配置，从命令行参数读取。
+   */
+  private option: IOption;
+
+  constructor(input: commander.CommanderStatic) {
+    const option: IOption = defaultOption;
+    if (input.typescript) {
+      option.typescript = true;
+    }
+    this.option = option;
+  }
+
+  public getOption() {
+    return this.option;
+  }
+}
+
+export { parser, WxappImageLoader, defaultOption };
