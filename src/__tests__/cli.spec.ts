2a8d6e3cdf496bd5f5183a2a6dea6de568f6d036
DiamondYuan <admin@diamondyuan.com>
2019-01-10 15:43:26 +0800

    W

--- src/__tests__/cli.spec.ts
+++ src/__tests__/cli.spec.ts
@@ -1,9 +1,9 @@
 import * as coffee from 'coffee';
 import * as path from 'path';
 
-describe('测试参数解析', () => {
+describe('', () => {
   // tslint:disable-next-line:typedef
-  it('测试参数解析的功能', done => {
+  it('', done => {
     coffee
       .fork(path.resolve(__dirname, '../bin/index.ts'))
       .debug()
